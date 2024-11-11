9e0e9770286ee03694963ea1d099166d37dfca4e
Paul Tarvydas <paul.tarvydas@gmail.com>
2024-11-11 16:41:27 -0500

    compile, run as separate subroutines

--- compile_and_run.py
+++ compile_and_run.py
@@ -5,10 +5,6 @@ import json
 
 import echo
 
-def initialize_hard_coded_test (arg):
-    palette = zd.initialize_component_palette ('.', '.', ['test.drawio.json'])
-    return [palette, ['.', '.', 'main', ['test.drawio.json'], arg]]
-
 def transpileDiagram (fname):
     ret = subprocess.run (['./das2json/mac/das2json', f'{fname}'], capture_output=True, encoding='utf-8')
     if  not (ret.returncode == 0):
@@ -19,15 +15,29 @@ def transpileDiagram (fname):
     else:
         return output.append ("stdout", ret.stdout)
 
+
+
+
+def initialize_hard_coded_test (arg):
+    palette = zd.initialize_component_palette ('.', '.', ['test.drawio.json'])
+    return [palette, ['.', '.', 'main', ['test.drawio.json'], arg]]
+
 def interpretDiagram (arg):
     [palette, env] = initialize_hard_coded_test (arg)
     echo.install (palette)
     zd.start (palette, env)
 
-def compile_and_run (filename, input_text):
-    output.reset ()
+def compile (filename):
     print (f'transpiling diagram {filename}')
     transpileDiagram (filename)
+    
+def run (filename, input_text):
     print (f'running diagram {filename} with input "{input_text}"')
+    output.reset ()
     interpretDiagram (input_text)
     return output.buffers
+    
+def compile_and_run (filename, input_text):
+    compile (filename)
+    r = run (filename, input_text)
+    return r
